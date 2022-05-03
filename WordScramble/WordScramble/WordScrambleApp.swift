466ebe0b0bbee6db43c0efcc1cb2d08b037ff4fb
raszion23 <rastazion23@gmail.com>
2022-05-03 18:20:59 +0200

    Created WordScramble project

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Raszion23 on 5/3/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

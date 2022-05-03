7bbaf128f3a16cd136f52fb93de40665ec93bcaa
raszion23 <rastazion23@gmail.com>
2022-05-03 19:21:08 +0200

    Created UI, consisting of NavigationView, TextField, and List. Added function to enter user's inputted word into list.

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -8,9 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Array of words already used
+    @State private var usedWords = [String]()
+    // Root word to spell other words from
+    @State private var rootWord = ""
+    // String that can be binded to a textfield
+    @State private var newWord = ""
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Section {
+                    // User input word to guess
+                    TextField("Enter your word", text: $newWord)
+                        .autocapitalization(.none)
+                }
+
+                Section {
+                    // Add guessed words to a list
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            // Show length of word using SF Symbols
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            // Rootword is the title
+            .navigationTitle(rootWord)
+
+            // Calls addNewWord() when user presses return on keyboard
+            .onSubmit {
+                addNewWord()
+            }
+        }
+    }
+
+    // Add user entry to usedWords array
+    func addNewWord() {
+        // Lowercase and trim the word, to make sure duplicate words with different cases are add
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+
+        // Exit if the remaining string is empty
+        guard answer.count > 0 else {
+            return
+        }
+
+        // Extra validation to come
+
+        // Insert the word at position 0 in usedWords array
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+
+        // Set newWord to an empty string
+        newWord = ""
     }
 }
 

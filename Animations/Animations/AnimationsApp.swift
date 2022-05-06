06e3607f019153f5a51f066c944fd360809fa828
raszion23 <rastazion23@gmail.com>
2022-05-06 19:17:39 +0200

    Created Animation project.

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Raszion23 on 5/6/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

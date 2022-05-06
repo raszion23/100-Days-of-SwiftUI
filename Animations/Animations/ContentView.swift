137cd667d53dc7ddb6d96680df0dda5ca7a0ee81
raszion23 <rastazion23@gmail.com>
2022-05-06 19:45:59 +0200

    Button animation.

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 1.0
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Tap Me") {
+//                    animationAmount += 1
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(.easeInOut(duration: 2)
+                    .repeatForever(autoreverses: false), value: animationAmount)
+        )
+        .onAppear {
+            animationAmount = 2 
+        }
     }
 }
 

7c4467864893cadd45328c52194a7cd514e2fa6f
raszion23 <rastazion23@gmail.com>
2022-05-12 18:10:52 +0200

    Created Edutainment project

---
+++ Edutainment/Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Raszion23 on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

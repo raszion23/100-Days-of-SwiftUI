290aae6b09141bfa320e17bc8094734a79232c17
raszion23 <rastazion23@gmail.com>
2022-05-15 20:24:57 +0200

    Created iExpense project

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Raszion23 on 5/15/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

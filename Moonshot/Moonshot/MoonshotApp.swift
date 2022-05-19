6300cc126949d2d3e5de2cdad7abab80af87c886
raszion23 <rastazion23@gmail.com>
2022-05-19 15:39:27 +0200

    Created Moonshot project

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Raszion23 on 5/19/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

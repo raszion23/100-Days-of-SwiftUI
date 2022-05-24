e841f0e124e0826409a7fbbc5c9f1c5249e1653c
raszion23 <rastazion23@gmail.com>
2022-05-24 17:22:17 +0200

    Showing mission details with ScrollView and GeometryReader. Merging Codable structs

---
+++ Moonshot/Moonshot/MissionView.swift
@@ -0,0 +1,118 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Raszion23 on 5/24/22.
+//
+
+import SwiftUI
+
+// Outline a the properties of a CrewMember
+struct CrewMember {
+    let role: String
+    let astronaut: Astronaut
+}
+
+struct MissionView: View {
+    // Create an instance of Mission struct
+    let mission: Mission
+    // Stores and array of CrewMembers
+    let crew: [CrewMember]
+
+    var body: some View {
+        // UI layout
+        GeometryReader { geometry in
+            ScrollView {
+                VStack {
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: geometry.size.width * 0.6)
+                        .padding(.top)
+
+                    Rectangle()
+                        .frame(height: 2)
+                        .foregroundColor(.lightBackground)
+                        .padding(.vertical)
+
+                    VStack(alignment: .leading) {
+                        Text("Mission Highlights")
+                            .font(.title.bold())
+                            .padding(.bottom, 5)
+
+                        Text(mission.description)
+
+                        Rectangle()
+                            .frame(height: 2)
+                            .foregroundColor(.lightBackground)
+                            .padding(.vertical)
+
+                        Text("Crew")
+                            .font(.title.bold())
+                            .padding(.bottom, 5)
+                    }
+                    .padding(.horizontal)
+
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            ForEach(crew, id: \.role) { crewMember in
+                                NavigationLink {
+                                    Text("Astronaut details")
+                                } label: {
+                                    HStack {
+                                        Image(crewMember.astronaut.id)
+                                            .resizable()
+                                            .frame(width: 104, height: 72)
+                                            .clipShape(Capsule())
+                                            .overlay(
+                                                Capsule()
+                                                    .strokeBorder(.white, lineWidth: 1))
+
+                                        VStack(alignment: .leading) {
+                                            Text(crewMember.astronaut.name)
+                                                .foregroundColor(.white)
+                                                .font(.headline)
+
+                                            Text(crewMember.role)
+                                                .foregroundColor(.secondary)
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                .padding(.bottom)
+            }
+        }
+        .navigationTitle(mission.displayName)
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.darkBackground)
+    }
+
+    // Accecpt that mission and astronauts. Figure out the array of resolved astronauts
+
+    init(mission: Mission, astronauts: [String: Astronaut]) {
+        self.mission = mission
+
+        self.crew = mission.crew.map { member in
+            if let astronaut = astronauts[member.name] {
+                return CrewMember(role: member.role, astronaut: astronaut)
+            } else {
+                fatalError("Missing \(member.name)")
+            }
+        }
+    }
+}
+
+struct MissionView_Previews: PreviewProvider {
+    // Passing in a Mission and astronaut object so the preview has something to render.
+    static let missions: [Mission] = Bundle.main.decode("missions.json")
+
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+
+    static var previews: some View {
+        MissionView(mission: missions[0], astronauts: astronauts)
+            .preferredColorScheme(.dark)
+    }
+}
